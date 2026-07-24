d13502cd99e8fa24298cf335ef74744cc8a4c1cf
intondiscott <intondiscott@gmail.com>
2026-07-24 11:04:28 -0400

    initial upload
    Co-authored-by: Scotty Intondi <intondiscott@gmail.com>
    Co-authored-by: Steven Ernest <sernest.james@gmail.com>

---
+++ index.js
@@ -0,0 +1,4 @@
+/**
+ * for each task(feature branch) you create a new branch from the main branch and then merge it back into the main branch when the task is complete.
+ * This way, you can work on multiple features simultaneously without interfering with each other's work.
+ * **/

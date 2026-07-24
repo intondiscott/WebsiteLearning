3bd347f8f08c61d2fd18e11ea7b40242f997801b
intondiscott <intondiscott@gmail.com>
2026-07-24 11:45:17 -0400

    Your First Task
    Co-authored-by: Scotty Intondi <intondiscott@gmail.com>
    Co-authored-by: Steven Ernest <sernest.james@gmail.com>

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
  * for each task(feature branch) you create a new branch from the main branch and then merge it back into the main branch when the task is complete.
  * This way, you can work on multiple features simultaneously without interfering with each other's work.
  * **/
+/**
+ * @param {HTMLElement} writeButton - The button element that triggers the write operation.
+ * @param {HTMLElement} readButton - The button element that triggers the read operation.
+ * @param {HTMLElement} deleteButton - The button element that triggers the delete operation.
+ *
+ */
